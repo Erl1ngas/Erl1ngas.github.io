d3c6e3dc73192a0b839d5ba0a80cbba588e67ea2
Erlingas <germanigonzalez@gmail.com>
2022-03-06 13:05:25 -0300

    header casi terminado

--- JS/navegador.js
+++ JS/navegador.js
@@ -1,7 +1,2 @@
-const lista = document.querySelectorAll(".lista");
 
-function activeLink(){
-    lista.forEach((item) => item.classList.remove("activa"));
-    this.classList.add("activa");
-}
-lista.forEach((item) => item.addEventListener("click",activeLink));
+
